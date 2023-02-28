83c862a0cc9ece25676de7add3dbdddb3706575d
sandeep <skjsan11@gmail.com>
2023-02-28 11:48:43 +0530

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const cors = require("cors")
+
+
 const app = express();
 const port = 5000;
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
